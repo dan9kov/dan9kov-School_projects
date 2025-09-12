69632f171153af25f17a31d6424a12390b6f125a
dan9kov <devyadan@yandex.ru>
2025-09-12 09:41:04 +0300

    add new lesson

---
+++ lesson_12_09_25/main_1.py
@@ -0,0 +1,13 @@
+arr = []
+def perevod(a, ss):
+    while (a >= ss):
+        arr.append(a % ss)
+        a //= ss
+    arr.append(a)
+
+x = 43 * 7 ** 103 - 21 * 7 ** 57 + 98
+perevod(x, 7)
+sm = 0
+for i in arr:
+    sm += i
+print(sm)
